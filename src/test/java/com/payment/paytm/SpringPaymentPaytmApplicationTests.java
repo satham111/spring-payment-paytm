73331929f9abe396f72b2a418ab9d03352004f2d
satha <satha@LAPTOP-PM7NHPEI>
2020-04-07 15:35:03 +0530

    Commit the entire folder

---
+++ src/test/java/com/payment/paytm/SpringPaymentPaytmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.paytm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaymentPaytmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
